c79a26a946391cc0a3e1dcaa99a1651e261b0ff6
dbierer <doug@unlikelysource.com>
2019-12-18 10:19:29 -0500

    2019-12-18 DB: modified ffi_c_function

--- ffi_c_function.php
+++ ffi_c_function.php
@@ -1,25 +1,12 @@
 <?php
-$func = <<<EOT
-#include <stdio.h>
-void bubble_sort(long list[], long n);
-void bubble_sort(long list[], long n) 
-{
-  long c, d, t;
-  for (c = 0 ; c < n - 1; c++) {
-    for (d = 0 ; d < n - c - 1; d++) {
-      if (list[d] > list[d+1]) {
-        /* Swapping */
-        t         = list[d];
-        list[d]   = list[d+1];
-        list[d+1] = t;
-      }
-    }
-  }
-}
-EOT;
-$ffi = FFI::cdef($func);
+set_include_path(__DIR__ . PATH_SEPARATOR . get_include_path());
+echo get_include_path();
+$ffi = FFI::cdef(
+    "void bubble_sort(long [], long);",
+    "bubble_sort.o");
 $array = FFI::new("long[1024]");
 // create array with random numbers
 for ($i = 0; $i < 10; $i++) $array[$i] = rand(0,9999);
 // display current state
 for ($i = 0; $i < count($array); $i++) echo $array[$i] . ',';
+
