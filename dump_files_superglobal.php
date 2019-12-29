79f053034efcfd284dcd8759132a1bb77a53c2a0
dbierer <doug@unlikelysource.com>
2019-12-28 23:46:04 -0500

    2019-12-29 DB: curl example

--- dump_files_superglobal.php
+++ dump_files_superglobal.php
@@ -1,3 +1,6 @@
 <?php
-var_dump($_SERVER);
+echo date('Y-m-d H:i:s') . ': ' . PHP_EOL;
+echo '$_FILES' . PHP_EOL;
 var_dump($FILES);
+echo PHP_EOL . '$_SERVER' . PHP_EOL;
+var_dump($_SERVER);
--- new_curl_stream.php
+++ new_curl_stream.php
@@ -3,7 +3,8 @@
 $ch = curl_init('http://localhost/dump_files_superglobal.php');
 
 // Create a CURLFile object
-$source = 'https://php-cl.com/img/doug_in_office_800px.png';
+$source = __DIR__ . '/test.txt';
+//$source = 'https://php-cl.com/img/doug_in_office_800px.png';
 $cfile = new CURLFile($source,'image/jpeg','test_name');
 
 // Assign POST data
@@ -11,7 +12,8 @@ $data = array('test_file' => $cfile);
 curl_setopt($ch, CURLOPT_POST,1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_NOPROGRESS, TRUE);
 
 // Execute the handle
 $response = curl_exec($ch);
-var_dump($response);
+echo $response;
