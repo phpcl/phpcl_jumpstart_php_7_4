decf676a340d5929524d2b3c0861f44fd74656cf
dbierer <doug@unlikelysource.com>
2020-01-08 21:32:23 -0500

    2020-01-09 DB: added examples for other bc breaks

--- bc_other_dateinterval.php
+++ bc_other_dateinterval.php
@@ -2,5 +2,8 @@
 // DateInterval Comparisons
 $di1 = new DateInterval('P90D');
 $di2 = new DateInterval('P2W');
+$di3 = new DateInterval('P2W');
 echo ($di1 == $di2) ? 'Not PHP 7.4' : 'PHP 7.4';
+echo "\n";
+echo ($di2 == $di3) ? 'Not PHP 7.4' : 'PHP 7.4';
 echo "\n";
--- bc_other_datetime.php
+++ bc_other_datetime.php
@@ -1,18 +1,18 @@
 <?php
-// taken from: https://bugs.php.net/bug.php?id=68406
-
+// see also: https://bugs.php.net/bug.php?id=68406
 $tz1 = new DateTimeZone('Asia/Bangkok');
 $tz2 = new DateTimeZone('Asia/Bangkok');
-
 $d = new DateTime('2020-01-01 01:01:01', $tz1);
-var_dump($d, $tz1, $tz2);
 
-echo ($tz2 == $d->getTimezone()) ? 'yes' : 'no';
-var_dump($d);
-echo "\n";
+echo "\nBefore:\n";
+$reflect = new ReflectionObject($d);
+echo $reflect . "\n";
 
-// DateInterval Comparisons
-$di1 = new DateInterval('P90D');
-$di2 = new DateInterval('P2W');
-echo ($di1 == $di2) ? 'Not PHP 7.4' : 'PHP 7.4';
-echo "\n";
+// in previous versions of PHP accessible props were left behind after
+// var_dump()
+var_dump($d->getTimezone(), $tz2);
+echo ($tz2 == $d->getTimezone()) ? 'YES' : 'NO'; 
+
+echo "\nAfter:\n";
+$reflect = new ReflectionObject($d);
+echo $reflect . "\n";
