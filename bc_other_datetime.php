6e89ecbb99de4de050c5e1ed94122241ac17209a
dbierer <doug@unlikelysource.com>
2020-01-08 21:54:20 -0500

    2020-01-09 DB: added examples for other bc breaks

--- bc_other_datetime.php
+++ bc_other_datetime.php
@@ -4,15 +4,22 @@ $tz1 = new DateTimeZone('Asia/Bangkok');
 $tz2 = new DateTimeZone('Asia/Bangkok');
 $d = new DateTime('2020-01-01 01:01:01', $tz1);
 
-echo "\nBefore:\n";
 $reflect = new ReflectionObject($d);
-echo $reflect . "\n";
+$str1 = $reflect->__toString();
 
 // in previous versions of PHP accessible props were left behind after
 // var_dump()
 var_dump($d->getTimezone(), $tz2);
 echo ($tz2 == $d->getTimezone()) ? 'YES' : 'NO'; 
+echo "\n"; 
 
-echo "\nAfter:\n";
 $reflect = new ReflectionObject($d);
-echo $reflect . "\n";
+$str2 = $reflect->__toString();
+
+// results
+echo "\nReflection before and after results: ";
+echo ($str1 == $str2) ? 'SAME' : 'DIFFERENT';
+echo "\n"; 
+
+echo "\nBefore:\n$str1\n";
+echo "\nAfter:\n$str2\n";
---
+++ bc_other_intl.php
@@ -0,0 +1,5 @@
+<?php
+$unicode = 'แอปเปิ้ล
+ดอทคอม';
+echo idn_to_ascii($unicode);
+echo "\n":
