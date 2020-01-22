e7026107ec70aa83974c3e808efc066d63042e72
dbierer <doug@unlikelysource.com>
2020-01-22 12:09:05 +0700

    2020-01-22 DB: added ArrayObject examples

---
+++ bc_spl_extends_array_object_combined.php
@@ -0,0 +1,25 @@
+<?php
+class Test extends ArrayObject
+{
+    public $id = 12345;
+    public $name = 'Cal Evans';
+    public function getVars()
+    {
+        return array_combine(
+            get_object_vars($this),
+            $this->getArrayCopy()
+        );
+    }
+}
+
+$test = new Test(['A' => 1,'B' => 2,'C' => 3]);
+
+// using `get_object_vars()` doesn't work in PHP 7.4
+var_dump($test->getVars());
+
+// using `getArrayCopy()` works OK
+var_dump($test->getArrayCopy());
+
+// introspecting object
+var_dump($test);
+
